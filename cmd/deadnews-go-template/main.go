e6997e81ff5fe82f1e1a882aa391ac1792978a4e
DeadNews <uhjnnn@gmail.com>
2022-09-06 15:37:15 +0200

    Update .goreleaser.yaml

--- cmd/deadnews-go-template/main.go
+++ cmd/deadnews-go-template/main.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"fmt"
+)
+
 // Get answer to the Meaning of Life, the Universe, and Everything.
 func GetAnswer() int {
 	result := 40 + 2
 	return result
+}
+
+func main() {
+	answer := GetAnswer()
+	fmt.Println(answer)
+	// Output: 42
 }
