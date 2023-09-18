c22ecdf0fca8be184ddc461528334cee0fd8d39f
DeadNews <uhjnnn@gmail.com>
2023-09-18 21:38:34 +0200

    feat: change the sample application (#33)
    * feat: change the sample application
    * ci: add `docker-publish`
    * ci: add `makefile`
    * ci(pre-commit): add `actionlint` hook
    * docs(README): add `image` info

--- cmd/deadnews-template-go/main.go
+++ cmd/deadnews-template-go/main.go
@@ -1,16 +1,61 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-// Get answer to the Meaning of Life, the Universe, and Everything.
-func GetAnswer() int {
-	result := 40 + 2
-	return result
+// This code is the main function of a Go program
+// that creates and starts a server using the Echo framework.
+// It retrieves the value of the "PORT" environment variable
+// and if it is not set, it defaults to port 1271.
+func main() {
+	// Create a new Echo instance.
+	e := makeServer()
+
+	// Get the value of the "PORT" environment variable.
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "1271"
+	}
+
+	// Start the server on the specified port.
+	e.Logger.Fatal(e.Start(":" + httpPort))
 }
 
-func main() {
-	fmt.Println(GetAnswer())
-	// Output: 42
+// makeServer creates a new instance of the Echo framework
+// and configures it with middleware for logging and error recovery.
+// It also defines two route handlers: one for the root ("/") route that returns an HTML response,
+// and another for the "/health" route that returns a JSON response.
+//
+// Returns:
+// - A configured instance of the Echo framework.
+func makeServer() *echo.Echo {
+	// Create a new Echo instance.
+	e := echo.New()
+
+	// Use middleware for logging and error recovery.
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Define the "/" route handler.
+	e.GET("/", handleRoot)
+
+	// Define the "/health" route handler.
+	e.GET("/health", handleHealth)
+
+	return e
+}
+
+// handleRoot handles the "/" route and returns an HTML response.
+func handleRoot(c echo.Context) error {
+	return c.HTML(http.StatusOK, "Hello, World!")
+}
+
+// handleHealth handles the "/health" route and returns a JSON response.
+func handleHealth(c echo.Context) error {
+	return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
 }
